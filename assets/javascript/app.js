772c1126bf970501a6326c3c92da5ae073dee61c
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-01-13 23:54:49 -0600

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function (){
-    
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+
+    console.log("..........")
 })
