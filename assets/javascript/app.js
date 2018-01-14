33a77d9e58a3d395961beb53581ef2efd9ed18ad
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-01-14 09:25:52 -0600

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,43 @@
 $(document).ready(function (){
 
-
-    $("#findgif").on("click", function(){
-        var keyword = $("#search").val();
-        console.log(keyword);
-            
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC";
-
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log(response);
+    $(".header-links").on("click", findgif)
+    
+    
 
 
-      for (var i = 0; i < 25; i++) {
+    var keyword = $("#search").val();
+    $("#findgif").on("click", findgif);
 
-        var gifImg = response.data[i].images.downsized_medium.url;
 
-        var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
+
+function findgif(keyword) {
+    event.preventDefault();
+    
+    $(".jiffyResults").empty();
+     
+    console.log(keyword);
         
-        $(".jiffyResults").append(newGif)
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC";
+
+$.ajax({
+  url: queryURL,
+  method: 'GET'
+}).done(function(response) {
+  console.log(response);
+
+
+  for (var i = 0; i < 25; i++) {
+
+    var gifImg = response.data[i].images.downsized_medium.url;
+
+    var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
     
-      }
-    
-    });
+    $(".jiffyResults").append(newGif)
 
+  }
 
-    })
-
-
+})
+}
 
 
 
