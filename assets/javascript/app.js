70b5f560f0f1f55ee333f3b0ee004fbf665f9ff0
ttejuosho <theycallmeflowz@yahoo.com>
2018-01-14 12:57:17 -0600

    onload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,32 @@
 $(document).ready(function (){
 
+  // <!-- On Page Load Show Trending gifs -->
+  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=1000";
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+    console.log(response);
+
+    $('.jiffyResults').endlessScroll({
+      inflowPixels: 300,
+      callback: function() {
+        for (var i = 0; i < 1000; i++) {
+        var gifImg = response.data[i].images.preview_gif.url;
+        var newGif = $('.jiffyResults img:nth-last-child(5)').clone();
+        $('.jiffyResults').append(newGif);
+      }}
+    });
+
+
+  });
+
+
+  
+
+
     $(".header-links").on("click", findgif)
-    
-    
 
 
     var keyword = $("#search").val();
@@ -24,8 +48,6 @@ $.ajax({
   method: 'GET'
 }).done(function(response) {
   console.log(response);
-
-
   for (var i = 0; i < 25; i++) {
 
     var gifImg = response.data[i].images.downsized_medium.url;
@@ -36,9 +58,9 @@ $.ajax({
 
   }
 
+
 })
 }
-
 
 
 })
