234e42456e74eadcd049c594dbec35a46820ba69
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-01-14 01:21:16 -0600

    indDOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,36 @@
 $(document).ready(function (){
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+
+    $("#findgif").on("click", function(){
+        var keyword = $("#search").val();
+        console.log(keyword);
+            
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC";
 
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function(response) {
       console.log(response);
+
+
+      for (var i = 0; i < 25; i++) {
+
+        var gifImg = response.data[i].images.downsized_medium.url;
+
+        var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
+        
+        $(".jiffyResults").append(newGif)
+    
+      }
+    
     });
 
-    console.log("..........")
+
+    })
+
+
+
+
+
 })
