468efbad2fdc54c2f2112f447a326119dc4a15a2
ttejuosho <theycallmeflowz@yahoo.com>
2018-01-14 21:02:15 -0600

    djt

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
 
   // <!-- On Page Load Show Trending gifs -->
-  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=100";
 
   $.ajax({
     url: queryURL,
@@ -10,7 +10,7 @@ $(document).ready(function (){
     console.log(response);
     for (var i = 0; i < 1000; i++) {
 
-      var gifImg = response.data[i].images.fixed_height_small.url;
+      var gifImg = response.data[i].images.fixed_height.url;
       var newGif = $("<img>").addClass("jiffy").attr("src", gifImg);
 
       // if gif is unavilable use our own image
@@ -39,7 +39,7 @@ $(document).ready(function (){
 
   
       // Ajax Call & query URL using the user input
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=10";
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=100";
       console.log("You clicked ", keyword)
       console.log(queryURL);
   
@@ -52,7 +52,7 @@ $(document).ready(function (){
     // run through all the results, get the image and save in a variable 
     for (var i = 0; i < 1000; i++) {
   
-      var gifImg = response.data[i].images.downsized_medium.url;
+      var gifImg = response.data[i].images.fixed_height.url;
   
       var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
       
@@ -84,7 +84,7 @@ function findgif(keyword) {
   var keyword = $("#search").val();
   
     // Ajax Call & query URL using the user input
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=100";
     console.log(queryURL);
 
 $.ajax({
@@ -96,7 +96,7 @@ $.ajax({
   // run through all the results, get the image and save in a variable 
   for (var i = 0; i < 1000; i++) {
 
-    var gifImg = response.data[i].images.downsized_medium.url;
+    var gifImg = response.data[i].images.fixed_height.url;
 
     var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
     
