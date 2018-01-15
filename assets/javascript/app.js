73474bdf53ba3a9ec2b25630d45031c1312fc66d
ttejuosho <theycallmeflowz@yahoo.com>
2018-01-14 20:31:31 -0600

    mvp

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,26 @@
 $(document).ready(function (){
 
   // <!-- On Page Load Show Trending gifs -->
-  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=1000";
+  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=10";
 
   $.ajax({
     url: queryURL,
     method: 'GET'
   }).done(function(response) {
     console.log(response);
+    for (var i = 0; i < 1000; i++) {
 
-    $('.jiffyResults').endlessScroll({
-      inflowPixels: 300,
-      callback: function() {
-        for (var i = 0; i < 1000; i++) {
-        var gifImg = response.data[i].images.preview_gif.url;
-        var newGif = $('.jiffyResults img:nth-last-child(5)').clone();
-        $('.jiffyResults').append(newGif);
-      }}
-    });
+      var gifImg = response.data[i].images.fixed_height_small.url;
+      var newGif = $("<img>").addClass("jiffy").attr("src", gifImg);
 
+      // if gif is unavilable use our own image
+      if (gifImg === undefined ){
+          gifImg = "./images/gifunavailable.gif";}
+     
+    // show new image on the page
+      $('.jiffyResults').append(newGif);
+
+    }
 
   });
 
@@ -26,29 +28,73 @@ $(document).ready(function (){
   
 
 
-    $(".header-links").on("click", findgif)
+
+    function headerLinkftn(keyword){
+      // Empty the Jiffy Results Div
+      $(".jiffyResults").empty();
+
+      var headerLinks = ["reactions", "entertainment", "sports", "stickers", "artists", "memes"];
+      
+      var keyword = $(this).text();
+
+  
+      // Ajax Call & query URL using the user input
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=10";
+      console.log("You clicked ", keyword)
+      console.log(queryURL);
+  
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+    console.log(response);
+  
+    // run through all the results, get the image and save in a variable 
+    for (var i = 0; i < 1000; i++) {
+  
+      var gifImg = response.data[i].images.downsized_medium.url;
+  
+      var newGif = $("<img>").addClass("jiffy").attr("src", gifImg)
+      
+      $(".jiffyResults").append(newGif)
+  
+    }
+  
+  
+  })
+      
+    }
 
 
-    var keyword = $("#search").val();
-    $("#findgif").on("click", findgif);
+  
+// When user clicks on a header Link run the header Link function
+    $("#header-links li").on("click", headerLinkftn)
+
+
+
 
 
 
 function findgif(keyword) {
-    event.preventDefault();
-    
+
+  // Empty the Jiffy Results Div
     $(".jiffyResults").empty();
      
-    console.log(keyword);
-        
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC";
+  // Saving the user input in a variable called keyword
+  var keyword = $("#search").val();
+  
+    // Ajax Call & query URL using the user input
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=10";
+    console.log(queryURL);
 
 $.ajax({
   url: queryURL,
   method: 'GET'
 }).done(function(response) {
   console.log(response);
-  for (var i = 0; i < 25; i++) {
+
+  // run through all the results, get the image and save in a variable 
+  for (var i = 0; i < 1000; i++) {
 
     var gifImg = response.data[i].images.downsized_medium.url;
 
@@ -62,5 +108,8 @@ $.ajax({
 })
 }
 
+
+// When user clicks on submit run the findgif function
+$("#findgif").on("click", findgif);
 
 })
