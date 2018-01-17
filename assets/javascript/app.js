045120a22d2bf0ec3ce95c5120840430001186f0
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-01-16 20:03:39 -0600

    no gifs found

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -97,9 +97,9 @@ $.ajax({
   for (var i = 0; i < 10; i++) {
 
     // if no gifs found Alert a message
-    // if (response.data = []){
-    //   gifImg = "./images/notfound.gif";
-    //   alert("No gifs found"); }
+    if (response.data = []){
+      gifImg = "./images/notfound.gif";
+      alert("No gifs found"); }
 
     //   // if gif is unavilable use our own image
     // } else if (gifImg === undefined){
