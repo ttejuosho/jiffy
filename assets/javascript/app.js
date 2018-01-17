51e15f2994b6e400ac50aafe769ec2bc9d942812
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-01-17 11:11:22 -0600

    removed pause and play feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,17 +10,17 @@ $(document).ready(function (){
     method: 'GET'
   }).done(function(response) {
     console.log(response);
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < 100; i++) {
 
-      var gifImg = response.data[i].images.fixed_height.url;
-      var newGif = $("<img>").addClass("jiffy").attr("src", gifImg);
+      var gifImgOnLoad = response.data[i].images.fixed_height.url;
+      var newGifOnLoad = $("<img>").addClass("jiffy").attr("src", gifImgOnLoad);
 
       // if gif is unavilable use our own image
-      if (gifImg === undefined ){
-          gifImg = "./images/gifunavailable.gif";}
+      if (gifImgOnLoad === undefined ){
+          gifImgOnLoad = "./images/gifunavailable.gif";}
      
     // show new image on the page
-      $('.jiffyResults').append(newGif);
+      $('.jiffyResults').append(newGifOnLoad);
 
     }
 
@@ -52,7 +52,7 @@ $(document).ready(function (){
     console.log(response);
   
     // run through all the results, get the image and save in a variable 
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < 100; i++) {
   
       var gifImg = response.data[i].images.fixed_height.url;
   
@@ -94,92 +94,59 @@ $.ajax({
   method: 'GET'
 }).done(function(response) {
   console.log(response);
-  
-  // run through all the results, get the image and save in a variable 
-  for (var i = 0; i < 10; i++) {
 
+    var gifImg;
     // if no gifs found Alert a message
-    if (response.data = []){
-      gifImg = "./images/notfound.gif";
-      alert("No gifs found"); }
-
-    //   // if gif is unavilable use our own image
-    // } else if (gifImg === undefined){
-    //   gifImg = "./images/gifunavailable.gif";
-    // }
-    
-    
-
-    var still = response.data[i].images.fixed_height_still.url;
-    var animated = response.data[i].images.fixed_height.url;
-     
+    if (response.data.length === 0){
+        var notFound = "https://i.makeagif.com/media/11-04-2015/mfnzwt.gif"
+        gifImg = $("<img>").attr("src", notFound);
+        
+        $(".jiffyResults").append(gifImg); 
+    }
 
 
-    // var shareDiv = $('<div>').addClass('fixed-action-btn horizontal click-to-toggle');
-    // var sharea = $('<a>').addClass('btn-floating btn-large red');
-    // var sharei = $('<i>').addClass('material-icons');
-    // var UL = $('<ul></ul>');
-    // var LI0 = $('<li>');
-    // var LI1 = $('<li>');
-    // var LI2 = $('<li>');
-    // var LI3 = $('<li>');
-    // var sociala0 = $('<a>').addClass('btn-floating red');
-    // var sociala1 = $('<a>').addClass('btn-floating yellow-darken-1');
-    // var sociala2 = $('<a>').addClass('btn-floating green');
-    // var sociala3 = $('<a>').addClass('btn-floating blue');
+  // run through all the results, get the image and save in a variable 
+  for (var i = 0; i < 100; i++) {
 
+// To make gifs pause and Play us lines 111 - 119 to define gifImg then 137 - 149
+    // var still = response.data[i].images.fixed_height_still.url;
+    // var animated = response.data[i].images.fixed_height.url;
 
-
-    var gifImg = $("<img>");
-    gifImg.addClass("jiffy");
-    gifImg.attr("src", still)
-    gifImg.attr("data-still", still);
-    gifImg.attr("data-animate", animated);
-    gifImg.attr("data-state", "still");
+    // var gifImg = $("<img>");
+    // gifImg.addClass("jiffy");
+    // gifImg.attr("src", still)
+    // gifImg.attr("data-still", still);
+    // gifImg.attr("data-animate", animated);
+    // gifImg.attr("data-state", "still");
   
-    
-    // shareDiv.append(sharea);
-    // sharea.html('<i class="material-icons">menu</i>');
+    // Create the image variable and point to src from the data Object
+    var gifImg = $("<img>");
+    gifImg.addClass("jiffy")
+    gifImg.attr("src", response.data[i].images.fixed_height.url);
 
-    // shareDiv.append(UL);
-    // UL.append(LI0);
-    // UL.append(LI1);
-    // UL.append(LI2);
-    // UL.append(LI3);
-    // LI0.append(sociala0);
-    // LI1.append(sociala1);
-    // LI2.append(sociala2);
-    // LI3.append(sociala3);
-    // sociala0.html('<i class="material-icons">instagram</i>');
-    // sociala1.html('<i class="material-icons">facebook</i>');
-    // sociala2.html('<i class="material-icons">twitter</i>');
-    // sociala3.html('<i class="material-icons">envelope</i>');
-
-
-    // $(".jiffyResults").append(shareDiv);
     $(".jiffyResults").append(gifImg);
-    
     
 
   }
+
 })
 }
 
 
-// Pause & Play Gifs
-$(document).on("click", ".jiffy", function() {
+// Pause & Play Gifs Activation
+// $(document).on("click", ".jiffy", function() {
 
-  var state = $(this).attr("data-state");
+//   var state = $(this).attr("data-state");
 
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  }
-  else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-  }
-});
+//   if (state === "still") {
+//     $(this).attr("src", $(this).attr("data-animate"));
+//     $(this).attr("data-state", "animate");
+//   }
+//   else {
+//     $(this).attr("src", $(this).attr("data-still"));
+//     $(this).attr("data-state", "still");
+//   }
+// });
 
 // When user clicks on submit run the findgif function
 $("#findgif").on("click", findgif);
